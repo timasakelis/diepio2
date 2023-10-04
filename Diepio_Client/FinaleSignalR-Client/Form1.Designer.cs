58f0457048de04232d2cb42c57714778a9c72440
timas-akelis <84728870+timas-akelis@users.noreply.github.com>
2023-10-04 14:06:07 +0300

    diepio with ignore

---
+++ Diepio_Client/FinaleSignalR-Client/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿using System.Collections.Generic;
+
+namespace FinaleSignalR_Client
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.openConnection = new System.Windows.Forms.Button();
+            this.messageInput = new System.Windows.Forms.TextBox();
+            this.sendMessage = new System.Windows.Forms.Button();
+            this.messages = new System.Windows.Forms.ListBox();
+            this.LeftTimer = new System.Windows.Forms.Timer(this.components);
+            this.RightTimer = new System.Windows.Forms.Timer(this.components);
+            this.UpTimer = new System.Windows.Forms.Timer(this.components);
+            this.DownTimer = new System.Windows.Forms.Timer(this.components);
+            this.ServerTimer = new System.Windows.Forms.Timer(this.components);
+            this.playerBoxes = new System.Windows.Forms.PictureBox[50];
+            this.SuspendLayout();
+            // 
+            // openConnection
+            // 
+            this.openConnection.Location = new System.Drawing.Point(785, 27);
+            this.openConnection.Name = "openConnection";
+            this.openConnection.Size = new System.Drawing.Size(151, 41);
+            this.openConnection.TabIndex = 0;
+            this.openConnection.Text = "Open Connection";
+            this.openConnection.UseVisualStyleBackColor = true;
+            this.openConnection.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // messageInput
+            // 
+            this.messageInput.Location = new System.Drawing.Point(637, 136);
+            this.messageInput.Name = "messageInput";
+            this.messageInput.Size = new System.Drawing.Size(445, 20);
+            this.messageInput.TabIndex = 1;
+            // 
+            // sendMessage
+            // 
+            this.sendMessage.Enabled = false;
+            this.sendMessage.Location = new System.Drawing.Point(796, 74);
+            this.sendMessage.Name = "sendMessage";
+            this.sendMessage.Size = new System.Drawing.Size(140, 43);
+            this.sendMessage.TabIndex = 2;
+            this.sendMessage.Text = "Send Message";
+            this.sendMessage.UseVisualStyleBackColor = true;
+            this.sendMessage.Click += new System.EventHandler(this.sendMessage_Click);
+            // 
+            // messages
+            // 
+            this.messages.FormattingEnabled = true;
+            this.messages.Location = new System.Drawing.Point(637, 179);
+            this.messages.Name = "messages";
+            this.messages.Size = new System.Drawing.Size(445, 173);
+            this.messages.TabIndex = 3;
+            // 
+            // LeftTimer
+            // 
+            this.LeftTimer.Interval = 5;
+            this.LeftTimer.Tick += new System.EventHandler(this.LeftTimer_Tick_1);
+            // 
+            // RightTimer
+            // 
+            this.RightTimer.Interval = 5;
+            this.RightTimer.Tick += new System.EventHandler(this.RightTimer_Tick_1);
+            // 
+            // UpTimer
+            // 
+            this.UpTimer.Interval = 5;
+            this.UpTimer.Tick += new System.EventHandler(this.UpTimer_Tick_1);
+            // 
+            // DownTimer
+            // 
+            this.DownTimer.Interval = 5;
+            this.DownTimer.Tick += new System.EventHandler(this.DownTimer_Tick);
+            // 
+            // ServerTimer
+            // 
+            this.ServerTimer.Interval = 10;
+            this.ServerTimer.Tick += new System.EventHandler(this.ServerTimer_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1147, 799);
+            this.Controls.Add(this.messages);
+            this.Controls.Add(this.sendMessage);
+            this.Controls.Add(this.messageInput);
+            this.Controls.Add(this.openConnection);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.KeyIsDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.KeyIsUp);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox[] playerBoxes;
+        private System.Windows.Forms.Button openConnection;
+        private System.Windows.Forms.TextBox messageInput;
+        private System.Windows.Forms.Button sendMessage;
+        private System.Windows.Forms.ListBox messages;
+        private System.Windows.Forms.Timer LeftTimer;
+        private System.Windows.Forms.Timer RightTimer;
+        private System.Windows.Forms.Timer UpTimer;
+        private System.Windows.Forms.Timer DownTimer;
+        private System.Windows.Forms.Timer ServerTimer;
+    }
+}
+
---
+++ Diepio_Client/FinaleSignalR-Client/Form1.cs
@@ -0,0 +1,276 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Runtime.Remoting.Messaging;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.AspNetCore.SignalR.Client;
+
+
+
+// Afk players dont go away so create Player class and all that jazz
+
+namespace FinaleSignalR_Client
+{
+    public partial class Form1 : Form
+    {
+        HubConnection connection;
+        int playerspeed;
+        int playerCount = 0;
+        int id;
+        PictureBox playerBox;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+
+            playerspeed = 5;
+            this.KeyPreview = true;
+
+            
+            
+            //((System.ComponentModel.ISupportInitialize)(Player)).EndInit();
+
+            //Conects to a given url
+            connection = new HubConnectionBuilder()
+                .WithUrl("https://localhost:7181/chatHub")
+                .WithAutomaticReconnect()
+                .Build();
+
+            // These define what happens during their scenarios
+            connection.Reconnecting += (sender) =>
+            {
+                messages.Invoke((MethodInvoker)delegate
+                {
+                    var newMessage = "Attempting to connect to the server...";
+                    messages.Items.Add(newMessage);
+                });
+
+                return Task.CompletedTask;
+            };
+
+            connection.Reconnected += (sender) =>
+            {
+                messages.Invoke((MethodInvoker)delegate
+                {
+                    var newMessage = "Reconnected to the server";
+                    messages.Items.Clear();
+                    messages.Items.Add(newMessage);
+                });
+
+                return Task.CompletedTask;
+            };
+
+            connection.Closed += (sender) =>
+            {
+                messages.Invoke((MethodInvoker)delegate
+                {
+                    var newMessage = "Connection Closed";
+                    messages.Items.Add(newMessage);
+                    openConnection.Enabled = true;
+                    sendMessage.Enabled = true;
+                });
+
+                return Task.CompletedTask;
+            };
+        }
+
+        private void createPlayer(string id)
+        {
+            var Player = new PictureBox();
+            Player.BackColor = System.Drawing.SystemColors.ControlDark;
+            Player.Location = new System.Drawing.Point(666, 422);
+            Player.Name = id;
+            Player.Size = new System.Drawing.Size(64, 64);
+            Player.TabIndex = 0;
+            Player.TabStop = false;
+            playerBoxes[playerCount] = Player;
+            this.Controls.Add(playerBoxes[playerCount]);
+            playerCount++;
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            connection.On<string, string>("ReceiveMessage", (user, message) => 
+            {
+                messages.Invoke((MethodInvoker)delegate
+                {
+                    var parsedMessage = message.Split('|');
+                    if (parsedMessage.Length > 0)
+                    {
+                        if (parsedMessage[0] == "All" || parsedMessage[0] == id.ToString())
+                        {
+                            switch (parsedMessage[1]) 
+                            {
+                                case "RequestAccepted":
+                                    createPlayer(id.ToString());
+                                    playerBox = playerBoxes[0];
+                                    ServerTimer.Start();
+                                    break;
+                                case "EnemyCreated":
+                                    if (parsedMessage[2] != id.ToString())
+                                    {
+                                        createPlayer(parsedMessage[2]);
+                                    }
+                                    break;
+                                case "Coords":
+                                    moveEnemy(parsedMessage[2], int.Parse(parsedMessage[3]), int.Parse(parsedMessage[4]));
+                                    break;
+                            }
+                        }
+                        
+                    }
+                    var newMessage = $"{user}: {message}";
+                    messages.Items.Add(newMessage);
+                });
+            });
+
+            try
+            {
+                await connection.StartAsync();
+                messages.Items.Add("Connection Started");
+                Random rnd = new Random();
+                id = rnd.Next(100000);
+                canICreateAvatar(id);
+
+                openConnection.Enabled = false;
+                sendMessage.Enabled = true;
+            }
+            catch (Exception ex)
+            {
+                messages.Items.Add(ex.Message);
+            }
+        }
+
+        private void moveEnemy(string id, int left, int top)
+        {
+            for (int i = 0; i < playerCount; i++)
+            {
+                if (playerBoxes[i].Name == id)
+                {
+                    playerBoxes[i].Left = left;
+                    playerBoxes[i].Top = top;
+                }
+            }
+        }
+
+        private async void canICreateAvatar(int id)
+        {
+            try
+            {
+                await connection.InvokeAsync("SendMessage", id.ToString(), "Request|CreateAvatar");
+            }
+            catch (Exception ex)
+            {
+                messages.Items.Add(ex.Message);
+            }
+        }
+
+        private async void sendMessage_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                await connection.InvokeAsync("SendMessage", id.ToString(), "Chat|"+messageInput.Text);
+            }
+            catch (Exception ex) 
+            {
+                messages.Items.Add(ex.Message);
+            }
+        }
+
+        private void playerBox_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void KeyIsDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left)
+            {
+                LeftTimer.Start();
+            }
+
+            if (e.KeyCode == Keys.Right)
+            {
+                RightTimer.Start();
+            }
+
+            if (e.KeyCode == Keys.Up)
+            {
+                UpTimer.Start();
+            }
+
+            if (e.KeyCode == Keys.Down)
+            {
+                DownTimer.Start();
+            }
+        }
+
+        private void KeyIsUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left)
+            {
+                LeftTimer.Stop();
+            }
+            if (e.KeyCode == Keys.Right)
+            {
+                RightTimer.Stop();
+            }
+            if (e.KeyCode == Keys.Up)
+            {
+                UpTimer.Stop();
+            }
+            if (e.KeyCode == Keys.Down)
+            {
+                DownTimer.Stop();
+            }
+        }
+        
+        private void DownTimer_Tick(object sender, EventArgs e)
+        {
+            
+            if (playerBox.Top < ClientSize.Height - playerBox.Height - 10)
+            {
+                playerBox.Top += playerspeed;
+            }
+        }
+
+        private void LeftTimer_Tick_1(object sender, EventArgs e)
+        {
+            if (playerBox.Left > 10)
+            {
+                playerBox.Left -= playerspeed;
+            }
+        }
+
+        private void UpTimer_Tick_1(object sender, EventArgs e)
+        {
+            if (playerBox.Top > 10)
+            {
+                playerBox.Top -= playerspeed;
+            }
+        }
+
+        private void RightTimer_Tick_1(object sender, EventArgs e)
+        {
+            if (playerBox.Left < ClientSize.Width - playerBox.Height - 10)
+            {
+                playerBox.Left += playerspeed;
+            }
+        }
+
+        private async void ServerTimer_Tick(object sender, EventArgs e)
+        {
+            try
+            {
+                Console.WriteLine(playerBox.Left.ToString());
+                await connection.InvokeAsync("SendMessage", id.ToString(), $"Coords|{playerBox.Left}|{playerBox.Top}");
+            }
+            catch (Exception ex)
+            {
+                messages.Items.Add(ex.Message);
+            }
+        }
+    }
+}
