0e7c8abef68b1b0e99c44062d0ab1095555356e9
FreeSpirit18 <99819303+FreeSpirit18@users.noreply.github.com>
2023-10-04 14:13:14 +0300

    Added map conrols

---
+++ Diepio_Client/FinaleSignalR-Client/Controls/MapControls.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FinaleSignalR_Client.Controls
+{
+    public class MapControl : Panel
+    {
+        public List<Rectangle> obstacles = new List<Rectangle>();
+        public MapControl()
+        {
+            //this.BackColor = Color.Green;
+            this.Size = new Size(800, 600);
+            this.BorderStyle = BorderStyle.FixedSingle;
+            obstacles.Add(new Rectangle(100, 100, 400, 10)); // Obstacle at (100, 100) with size 50x50
+            obstacles.Add(new Rectangle(200, 200, 10, 90));
+            obstacles.Add(new Rectangle(500, 300, 10, 200));
+            obstacles.Add(new Rectangle(100, 400, 100, 10));
+
+        }
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+
+            // Create a graphics object to draw on the map control
+            Graphics g = e.Graphics;
+
+            // Draw each obstacle in the obstacles list
+            foreach (Rectangle obstacle in obstacles)
+            {
+                g.FillRectangle(Brushes.Gray, obstacle);
+            }
+        }
+
+    }
+}
