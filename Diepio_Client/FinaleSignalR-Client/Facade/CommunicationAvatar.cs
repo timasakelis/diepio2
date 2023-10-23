98a798243fe2eb1898ffef21052d162b4a529747
Rokas666666 <60842961+Rokas666666@users.noreply.github.com>
2023-10-23 22:17:11 +0300

    added the facade pattern through creating Communication subsystems

---
+++ Diepio_Client/FinaleSignalR-Client/Facade/CommunicationAvatar.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FinaleSignalR_Client.Web
+{
+    public class CommunicationAvatar
+    {
+        HubConnection connection;
+        string id;
+        ListBox messages;
+
+        public CommunicationAvatar(HubConnection hub, string id, ListBox messages)
+        {
+            this.connection = hub;
+            this.id = id;
+            this.messages = messages;
+        }
+
+        public async void canICreateAvatar(string pClass)
+        {
+            try
+            {
+                await connection.InvokeAsync("SendMessage", id, "Request|CreateAvatar|" + pClass);
+            }
+            catch (Exception ex)
+            {
+                messages.Items.Add(ex.Message);
+            }
+        }
+
+        public async void RemoveAvatar(string text)
+        {
+            try
+            {
+                await connection.InvokeAsync("SendMessage", id, "Remove|" + text);
+            }
+            catch (Exception ex)
+            {
+                messages.Items.Add(ex.Message);
+            }
+        }
+    }
+}
