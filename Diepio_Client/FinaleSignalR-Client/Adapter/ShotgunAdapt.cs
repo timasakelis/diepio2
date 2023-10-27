525730135de625308022fea1431253755dc70fe8
FreeSpirit18 <99819303+FreeSpirit18@users.noreply.github.com>
2023-10-27 16:14:39 +0300

    fix for shotgun

--- Diepio_Client/FinaleSignalR-Client/Adapter/ShotgunAdapt.cs
+++ Diepio_Client/FinaleSignalR-Client/Adapter/ShotgunAdapt.cs
@@ -22,21 +22,32 @@ namespace FinaleSignalR_Client.Adapter
         public void SpeedBoost()
         {
             if (this.ShotGun.Speed < 5)
+            {
                 this.ShotGun.Speed++;
+                Refresh();
+            }
         }
 
         public void SizeBoost()
         {
-            if (this.ShotGun.Size < 10)
+            if (this.ShotGun.Size < 8)
+            {
                 this.ShotGun.Size++;
+                Refresh();
+            }
         }
 
         public void Default()
         {
             this.ShotGun.Size = 0;
             this.ShotGun.Speed = 0;
+            Refresh();
         }
-
+        public void Refresh()
+        {
+            this.Size = this.ShotGun.Size;
+            this.Speed = this.ShotGun.Speed;
+        }
         public List<IBullet> Fire(int x, int y, Vector2 Direction, string id)
         {
             List<IBullet> bullets = this.ShotGun.Shoot(x, y, Direction, id);
